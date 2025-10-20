7c51edd70d747529cfea44784d3f8de6012960b1
anuja-28 <anuamale19@gmail.com>
2025-10-20 23:37:49 +0530

    Commit by anuja on 2/10/2025

--- springapp/src/main/java/com/baseproject/springapp/controller/ViewPageController.java
+++ springapp/src/main/java/com/baseproject/springapp/controller/ViewPageController.java
@@ -1,4 +1,4 @@
-package com.baseproject.controller;
+package com.baseproject.springapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +8,15 @@ public class ViewPageController {
 
     @GetMapping("/")
     public String home() {
-        // model.addAttribute("users", repo.findAll());
-        // model.addAttribute("user", new User());
-        // model.addAttribute("customer", new Customer());
+       
          System.out.println("✅ Home page called");
         return "index";
+    }
+
+     @GetMapping("/manageExpenses")
+    public String exp_dash() {
+        System.out.println("✅ Expense dashboard page called");
+        return "manage_expense_dash";
     }
 
 }
